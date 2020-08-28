13c0e5c80065dd0715da82476a8cdacf18a9d1f3
Naoki Yoshida <1711140006e@ed.fuk.kindai.ac.jp>
2020-08-28 16:46:03 +0900

    first commit
    first

---
+++ Assets/SelectCompany.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectCompany : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int a = 1;
+        int b = 2;
+        int c = a + b;
+        Debug.Log("text");
+        Debug.Log(c);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnClick()
+    {
+        Debug.Log ("クリックされた");
+    }
+}
