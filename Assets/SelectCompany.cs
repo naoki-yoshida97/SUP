de438635dfbc7bf4e4360d4007b940a3795346d9
Naoki Yoshida <1711140006e@ed.fuk.kindai.ac.jp>
2020-09-18 17:16:12 +0900

    change the script

--- Assets/SelectCompany.cs
+++ Assets/SelectCompany.cs
@@ -1,26 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SelectCompany : MonoBehaviour
 {
+    private Text SumCom;
+    private int income = 0;
     // Start is called before the first frame update
     void Start()
     {
-        int a = 1;
-        int b = 2;
-        int c = a + b;
-        Debug.Log("text");
-        Debug.Log(c);
+        // int a = 1;
+        // int b = 2;
+        // int c = a + b;
+        // Debug.Log("text");
+        // Debug.Log(c);
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // void Update()
+    // {
+    // }
+
     public void OnClick()
     {
-        Debug.Log ("クリックされた");
+        Debug.Log ("クリックされた");//push the button
+        
+
+
     }
 }
