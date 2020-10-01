ed0d3207cc3dbf9960fb1097771831a70c75c970
Naoki Yoshida <1711140006e@ed.fuk.kindai.ac.jp>
2020-10-01 18:37:54 +0900

    chang for test

--- Assets/SelectCompany.cs
+++ Assets/SelectCompany.cs
@@ -7,6 +7,7 @@ public class SelectCompany : MonoBehaviour
 {
     private Text SumCom;
     private int income = 0;
+   
     // Start is called before the first frame update
     void Start()
     {
@@ -25,8 +26,10 @@ public class SelectCompany : MonoBehaviour
     public void OnClick()
     {
         Debug.Log ("クリックされた");//push the button
+        this.SumCom = GameObject.Find("sumcomp").GetComponent<Text>();
+         // this.C_text = GameObject.Find("Text").GetComponent<Text>(); // textコンポーネントを取得
+        SumCom.text = income.ToString() +" 万円"; // int型をstring型に変換
+        income += 50; 
         
-
-
     }
 }
