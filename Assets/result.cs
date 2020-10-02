02874a732ca61cae01485acc05e83f4eafe66178
Naoki Yoshida <1711140006e@ed.fuk.kindai.ac.jp>
2020-10-02 16:25:55 +0900

    add sucripts

--- Assets/result.cs
+++ Assets/result.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class result : MonoBehaviour
-{
+{   
+    public Text goukei;
+    GameObject sc;
+
+    SelectCompany script;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // goukei.text = "hogehoge";
+        sc = GameObject.Find("sumcomp");
+        script = sc.GetComponent<SelectCompany>();
+
     }
 
     // Update is called once per frame
