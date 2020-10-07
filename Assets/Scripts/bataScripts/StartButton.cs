0ba0a9fe12892d14fe29c22461640a48a55a4663
junya <>
2020-10-07 15:36:28 +0900

    move_scene1

---
+++ Assets/Scripts/bataScripts/StartButton.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+ 
+public class StartButton : MonoBehaviour {
+ 
+    public void OnClickStartButton()
+    {
+        SceneManager.LoadScene("CityBoard");
+    }
+ 
+}
