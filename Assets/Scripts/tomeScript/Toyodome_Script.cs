96bbf6a50a1df7824b5fd96479b5f0e27fc07a40
Toyodome <71068370+Toyodome@users.noreply.github.com>
2020-10-09 16:47:15 +0900

    会社カード削除及び削除用ドロップダウンリストの動的削除機能実装

--- Assets/Scripts/tomeScript/Toyodome_Script.cs
+++ Assets/Scripts/tomeScript/Toyodome_Script.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Toyodome_Script : MonoBehaviour {
-    int pos_x = -200; //-320
-    int pos_y = 85; //150
+    int pos_x = 320; //-320
+    int pos_y = -30; //150
     int num;
-    List<string> corp_name = new List<string> ();
+    public static List<string> corp_name_list = new List<string> ();
     [SerializeField] private Dropdown dropdown; //Dropdownを格納する変数
     [SerializeField] private Dropdown dropdown_delete; //Dropdownを格納する変数
 
@@ -35,42 +35,52 @@ public class Toyodome_Script : MonoBehaviour {
         "動画制作",
         "IT企業"
     };
+    //同一企業カードの数表示用
+    int[] corp_cnt = new int[21] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 
     private GameObject corp; //会社を格納する変数
     //ボタンが押された場合、今回呼び出される関数
-    public void OnClickAdd () {
+    public void CorpClickAdd () {
         num++;
-        // Debug.Log ("Add"); //ログを出力
-        string corp_num = dropdown.value.ToString ();
+        string name;
         // listにオブジェクト名を格納
-        corp_name.Add (corp_num);
+        name = dropdown.value.ToString () + "_" + corp_cnt[dropdown.value];
+        corp_name_list.Add (name);
+        corp_cnt[dropdown.value]++;
         // ドロップダウンに会社名を追加
-        dropdown_delete.options.Add (new Dropdown.OptionData { text = corpAry[dropdown.value] });
+        dropdown_delete.options.Add (new Dropdown.OptionData { text = corpAry[dropdown.value] + corp_cnt[dropdown.value] });
         // 器になるゲームオブジェクトを作成
         // 引数はオブジェクト名
-        GameObject corp = new GameObject (corp_name[corp_name.Count - 1]);
+        GameObject corp = new GameObject (corp_name_list[corp_name_list.Count - 1]);
         // 作ったゲームオブジェクトをCanvasの子にする
         corp.transform.parent = GameObject.Find ("Canvas").transform;
-        // 画像のアンカーポジションを追加して画面の真ん中に
+        // 画像のアンカーポジションを追加
         corp.AddComponent<RectTransform> ().anchoredPosition = new Vector3 (pos_x, pos_y, 0);
-        // 縮尺を糖倍にする
-        corp.GetComponent<RectTransform> ().localScale = new Vector3 (1, 1, 1);
+        // 縮尺を変更
+        corp.GetComponent<RectTransform> ().localScale = new Vector3 (0.6f, 0.6f, 0.6f);
         // スプライト画像追加
-        corp.AddComponent<Image> ().sprite = Resources.Load<Sprite> (corp_num);
+        corp.AddComponent<Image> ().sprite = Resources.Load<Sprite> (dropdown.value.ToString ());
         // アスペクト比を元画像と同じサイズにする
         corp.GetComponent<Image> ().preserveAspect = true;
         // 画像のwidthとhightを変更
         //corp.sizeDelta = new Vector2 (50.0f, 50.0f);
-        pos_x = pos_x + 80;
+        pos_x = pos_x + 55;
         if (num == 6) {
-            pos_y = pos_y - 105;
-            pos_x = -200;
+            pos_y = pos_y - 65;
+            pos_x = 320;
         }
         dropdown_delete.RefreshShownValue ();
     }
 
-    public void OnClickDelete () {
+    public void CorpClickDelete () {
+        GameObject obj = GameObject.Find (corp_name_list[dropdown_delete.value]);
+        Destroy (obj);
+        corp_name_list.RemoveAt (dropdown_delete.value);
+        dropdown_delete.options.RemoveAt (dropdown_delete.value);
 
+        //会社カードcorp_name_listの中身を全部削除
+
+        //削除したdropdown_delete.value以外のcorp_name_listを全部再生成
     }
 
 }
