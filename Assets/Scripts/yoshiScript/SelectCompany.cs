3f357a0a850952ebb5e15ba8ead43fcf5bd5a702
Naoki Yoshida <1711140006e@ed.fuk.kindai.ac.jp>
2020-10-16 11:33:07 +0900

    change files positions

--- Assets/Scripts/yoshiScript/SelectCompany.cs
+++ Assets/Scripts/yoshiScript/SelectCompany.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class SelectCompany : MonoBehaviour
-{
+{   
+    //定義
     private Text SumCom;
     public int income = 0;
    
     // Start is called before the first frame update
     void Start()
     {
+        //ここでオブジェクトの指定と表記
         this.SumCom = GameObject.Find("sumcomp").GetComponent<Text>();
         SumCom.text = income.ToString()+" 万円";
 
@@ -18,7 +20,7 @@ public class SelectCompany : MonoBehaviour
 
     public void OnClick()
     {
-        // Debug.Log ("クリックされた");//push the button
+        //ボタンを押した時の処理
         income += 50;
         SumCom.text = income.ToString() +" 万円"; // int型をstring型に変換
          
