e26691742d1d41e64a36bfe440c90c8497eabfa0
Naoki Yoshida <1711140006e@ed.fuk.kindai.ac.jp>
2020-10-16 17:47:11 +0900

    add script files

---
+++ Assets/Scripts/yoshiScript/addeikyo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class addeikyo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    
+
+    // Pudh Object:eip button when it starts
+    public void OnClick()
+    {
+        Debug.Log ("クリックされた");
+    }
+
+}
