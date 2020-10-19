99226a3e70a36c86c5a62341d6c2617845ac9d76
Toyodome <71068370+Toyodome@users.noreply.github.com>
2020-10-19 19:58:08 +0900

    役員カードと都市カードのAdd/Delete機能追加

--- Assets/Scripts/tomeScript/CityCreate.cs
+++ Assets/Scripts/tomeScript/CityCreate.cs
@@ -3,46 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class OfficerCreate : MonoBehaviour {
+public class CityCreate : MonoBehaviour {
     static int pos_x = 320;
-    static int pos_y = -160;
+    static int pos_y = -225;
     static int num;
-    public static List<string> officer_name_list = new List<string> ();
+    public static List<string> city_name_list = new List<string> ();
     [SerializeField] private Dropdown dropdown; //Dropdownを格納する変数
     [SerializeField] private Dropdown dropdown_delete; //Dropdownを格納する変数
 
     // 会社名配列
-    string[] corpAry = new string[9] {
-        "マーケティング",
-        "企画",
-        "SE",
-        "マネージャー",
-        "人事",
-        "事務",
-        "経理",
-        "PG",
-        "営業"
+    string[] corpAry = new string[4] {
+        "都市カードA",
+        "都市カードB",
+        "都市カードC",
+        "都市カードD"
     };
     //同一企業カードの数表示用
-    int[] corp_cnt = new int[9] { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+    int[] corp_cnt = new int[4] { 0, 0, 0, 0 };
 
     private GameObject corp; //会社を格納する変数
     //ボタンが押された場合、今回呼び出される関数
-    public void OfficerClickAdd () {
+    public void CityClickAdd () {
         num++;
         string name;
-        int officer_card_num;
-        officer_card_num = dropdown.value + 54;
+        int city_card_num;
+        city_card_num = dropdown.value + 64;
         // listにオブジェクト名を格納
-        name = officer_card_num.ToString () + "_" + corp_cnt[dropdown.value];
-        officer_name_list.Add (name);
+        name = city_card_num.ToString () + "_" + corp_cnt[dropdown.value];
+        city_name_list.Add (name);
         corp_cnt[dropdown.value]++;
         // ドロップダウンに会社名を追加
         //dropdown_delete.options.Add (new Dropdown.OptionData { text = corpAry[dropdown.value] + corp_cnt[dropdown.value] });
         dropdown_delete.options.Add (new Dropdown.OptionData { text = corpAry[dropdown.value] });
         // 器になるゲームオブジェクトを作成
         // 引数はオブジェクト名
-        GameObject corp = new GameObject (officer_name_list[officer_name_list.Count - 1]);
+        GameObject corp = new GameObject (city_name_list[city_name_list.Count - 1]);
         // 作ったゲームオブジェクトをCanvasの子にする
         corp.transform.parent = GameObject.Find ("Canvas").transform;
         // 画像のアンカーポジションを追加
@@ -50,7 +45,7 @@ public class OfficerCreate : MonoBehaviour {
         // 縮尺を変更
         corp.GetComponent<RectTransform> ().localScale = new Vector3 (0.6f, 0.6f, 0.6f);
         // スプライト画像追加
-        corp.AddComponent<Image> ().sprite = Resources.Load<Sprite> (officer_card_num.ToString ());
+        corp.AddComponent<Image> ().sprite = Resources.Load<Sprite> (city_card_num.ToString ());
         // アスペクト比を元画像と同じサイズにする
         corp.GetComponent<Image> ().preserveAspect = true;
         // 画像のwidthとhightを変更
@@ -63,27 +58,27 @@ public class OfficerCreate : MonoBehaviour {
         dropdown_delete.RefreshShownValue ();
     }
 
-    public void OfficerClickDelete () {
+    public void CityClickDelete () {
         num--;
-        //会社カードofficer_name_listの中身を全部削除
-        for (int i = 0; i < officer_name_list.Count; i++) {
+        //会社カードcity_name_listの中身を全部削除
+        for (int i = 0; i < city_name_list.Count; i++) {
             Debug.Log (i);
-            GameObject obj = GameObject.Find (officer_name_list[i]);
+            GameObject obj = GameObject.Find (city_name_list[i]);
             Destroy (obj);
         }
         Debug.Log (dropdown_delete.value);
-        officer_name_list.RemoveAt (dropdown_delete.value);
+        city_name_list.RemoveAt (dropdown_delete.value);
         dropdown_delete.options.RemoveAt (dropdown_delete.value);
 
-        //削除したdropdown_delete.value以外のofficer_name_listを全部再生成
+        //削除したdropdown_delete.value以外のcity_name_listを全部再生成
         pos_x = 320; //-320
-        pos_y = -160; //150
-        for (int i = 0; i < officer_name_list.Count; i++) {
-            string[] arr = officer_name_list[i].Split ('_');
+        pos_y = -225; //150
+        for (int i = 0; i < city_name_list.Count; i++) {
+            string[] arr = city_name_list[i].Split ('_');
             Debug.Log (arr[0]);
             // 器になるゲームオブジェクトを作成
             // 引数はオブジェクト名
-            GameObject corp = new GameObject (officer_name_list[i]);
+            GameObject corp = new GameObject (city_name_list[i]);
             // 作ったゲームオブジェクトをCanvasの子にする
             corp.transform.parent = GameObject.Find ("Canvas").transform;
             // 画像のアンカーポジションを追加
