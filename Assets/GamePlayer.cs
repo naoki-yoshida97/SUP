ecf30665a622bfd54b79b4f5e6645b3798547cea
Toyodome <71068370+Toyodome@users.noreply.github.com>
2020-10-24 19:55:25 +0900

    PhotonServerいろいろいろ

---
+++ Assets/GamePlayer.cs
@@ -0,0 +1,14 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+// MonoBehaviourPunCallbacksを継承すると、photonViewプロパティが使えるようになる
+public class GamePlayer : MonoBehaviourPunCallbacks {
+    private void Update () {
+        // 自身が生成したオブジェクトだけに移動処理を行う
+        if (photonView.IsMine) {
+            var dx = 0.1f * Input.GetAxis ("Horizontal");
+            var dy = 0.1f * Input.GetAxis ("Vertical");
+            transform.Translate (dx, dy, 0f);
+        }
+    }
+}
---
+++ Assets/SampleScene.cs
@@ -0,0 +1,24 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+// MonoBehaviourではなくMonoBehaviourPunCallbacksを継承して、Photonのコールバックを受け取れるようにする
+public class SampleScene : MonoBehaviourPunCallbacks {
+    private void Start () {
+        // PhotonServerSettingsに設定した内容を使ってマスターサーバーへ接続する
+        PhotonNetwork.ConnectUsingSettings ();
+    }
+
+    // マスターサーバーへの接続が成功した時に呼ばれるコールバック
+    public override void OnConnectedToMaster () {
+        // "room"という名前のルームに参加する（ルームが無ければ作成してから参加する）
+        PhotonNetwork.JoinOrCreateRoom ("room", new RoomOptions (), TypedLobby.Default);
+    }
+
+    // マッチングが成功した時に呼ばれるコールバック
+    public override void OnJoinedRoom () {
+        // マッチング後、ランダムな位置に自分自身のネットワークオブジェクトを生成する
+        var v = new Vector3 (Random.Range (-3f, 3f), Random.Range (-3f, 3f));
+        PhotonNetwork.Instantiate ("GamePlayer", v, Quaternion.identity);
+    }
+}
