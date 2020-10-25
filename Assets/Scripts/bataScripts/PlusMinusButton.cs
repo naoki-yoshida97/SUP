aa4acf04b671cf23420e45b1c78db124475bdd93
junya <>
2020-10-25 21:01:07 +0900

    プラマイ後は配置だけ

---
+++ Assets/Scripts/bataScripts/PlusMinusButton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Photon.Pun;
+using Photon.Realtime;
+public class PlusMinusButton : MonoBehaviour
+{
+    public Text text;
+    public int num = 0;
+
+    void Start(){
+        text = text.GetComponent<Text> ();
+    }
+
+    public void OnClickPlus(){
+        if(num < 7){
+            num+=1;
+        }
+        text.text = num.ToString();
+        Debug.Log("プラス");  // ログを出力
+    }
+
+    public void OnClickMinus(){
+        if(num > 0){
+            num-=1;
+        }
+        text.text = num.ToString();
+        Debug.Log("マイナス");  // ログを出力
+    }
+}
