c5c8b6cf454d43455bea9cdc3d5f6a53fbc343b5
Toyodome <71068370+Toyodome@users.noreply.github.com>
2020-10-25 18:20:38 +0900

    Photonnetwork.JoinRoomをLobby_Button_scriptに追加

--- Assets/Scripts/tomeScript/Lobby_Button_script.cs
+++ Assets/Scripts/tomeScript/Lobby_Button_script.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
+using Photon.Realtime;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Lobby_Button_script : MonoBehaviour {
     public void OnClickRoom1Button () {
+        PhotonNetwork.JoinRoom ("Room1");
+
         SceneManager.LoadScene ("CityBoard");
         Application.LoadLevelAdditive ("CorpCreate");
         Application.LoadLevelAdditive ("SampleScene");
