175634882c7392f9e1872e895ddd63b843e4ec84
Toyodome <71068370+Toyodome@users.noreply.github.com>
2020-10-25 12:59:17 +0900

    シーンの複数読込

---
+++ Assets/Scripts/tomeScript/Lobby_Button_script.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Lobby_Button_script : MonoBehaviour {
+    public void OnClickRoom1Button () {
+        SceneManager.LoadScene ("CityBoard");
+        Application.LoadLevelAdditive ("CorpCreate");
+        Application.LoadLevelAdditive ("SampleScene");
+    }
+}
