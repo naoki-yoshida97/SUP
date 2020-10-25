98881f7528ca7283e563415f207204070c6a3bf0
Toyodome <71068370+Toyodome@users.noreply.github.com>
2020-10-25 14:42:09 +0900

    都市カード作成

---
+++ Assets/PhotonManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotonManager : Photon.MonoBehaviour {
+    // Start is called before the first frame update
+    void Start () {
+
+    }
+
+    void ConnectPhoton () {
+        PhotonNetwork.ConnectUsingSettings ("v1.0");
+    }
+
+    void OnJoinedLobby () {
+        Debug.Log ("PhotonManager OnJoinedLobby");
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+}
