6710f5d358b5f62b1ff1ec98fbdde526d578021f
junya <>
2020-10-25 14:24:44 +0900

    スタートボタンとロビー調整

--- Assets/SampleInput.cs
+++ Assets/SampleInput.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class SampleInput : MonoBehaviour {
 
@@ -17,13 +18,12 @@ public class SampleInput : MonoBehaviour {
 
     // ボタンが押された場合、今回呼び出される関数
     public void OnClick(){
-        ///Debug.Log("押された!");  // ログを出力
+        Debug.Log("押された!");  // ログを出力
         text.text = inputField.text;
     }
 
-    public void InputText(){
-        //テキストにinputFieldの内容を反映
-        ///text.text = inputField.text;
+    public void OnClickStartButton () {
+        SceneManager.LoadScene ("Lobby");
     }
 
 }
