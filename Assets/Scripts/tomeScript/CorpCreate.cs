11ffc409521b6aaffdef25117c64dc16b930c6a8
Toyodome <71068370+Toyodome@users.noreply.github.com>
2020-10-25 17:30:10 +0900

    各種カード生成の位置調整、作成上限の設定

--- Assets/Scripts/tomeScript/CorpCreate.cs
+++ Assets/Scripts/tomeScript/CorpCreate.cs
@@ -15,27 +15,27 @@ public class CorpCreate : MonoBehaviour {
 
     // 会社名配列
     string[] corpAry = new string[21] {
-        "建築会社",             //0
-        "ゲストハウス",         //1
-        "コンビニ",             //2
-        "ファミレス",           //3
-        "カフェ",               //4
-        "アパレル",             //5
-        "インフルエンサー",       //6
-        "農業",                 //7
-        "保険代理店",            //8
-        "薬局",                 //9
-        "新電力会社",            //10
-        "製薬会社",              //11
-        "不動産",               //12
-        "バイオマス",            //13
-        "TV局",                 //14
-        "WEB製作",              //15
-        "ロボティクス",          //16
-        "PGスクール",           //17
-        "広告代理店",           //18
-        "動画制作",             //19
-        "IT企業"                //20
+        "建築会社", //0
+        "ゲストハウス", //1
+        "コンビニ", //2
+        "ファミレス", //3
+        "カフェ", //4
+        "アパレル", //5
+        "インフルエンサー", //6
+        "農業", //7
+        "保険代理店", //8
+        "薬局", //9
+        "新電力会社", //10
+        "製薬会社", //11
+        "不動産", //12
+        "バイオマス", //13
+        "TV局", //14
+        "WEB製作", //15
+        "ロボティクス", //16
+        "PGスクール", //17
+        "広告代理店", //18
+        "動画制作", //19
+        "IT企業" //20
     };
     //同一企業カードの数表示用
     int[] corp_cnt = new int[21] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
@@ -44,50 +44,55 @@ public class CorpCreate : MonoBehaviour {
 
     //ボタンが押された場合、今回呼び出される関数
     public void CorpClickAdd () {
-        num++;
-        string name;
+        if (num == 12) {
+            Debug.Log ("CorpCreate: これ以上生成できません");
+        } else {
+            num++;
+            string name;
 
-        //delete drop down~~~~~~
-        // listにオブジェクト名を格納 num_cnt
-        name = dropdown.value.ToString () + "_" + corp_cnt[dropdown.value];
+            //delete drop down~~~~~~
+            // listにオブジェクト名を格納 num_cnt
+            name = dropdown.value.ToString () + "_" + corp_cnt[dropdown.value];
 
-        //会社番号(0,1,2...)_その枚数
-        corp_name_list.Add (name);
-        corp_cnt[dropdown.value]++;
+            //会社番号(0,1,2...)_その枚数
+            corp_name_list.Add (name);
+            corp_cnt[dropdown.value]++;
 
-        // ドロップダウンに会社名を追加
-        //dropdown_delete.options.Add (new Dropdown.OptionData { text = corpAry[dropdown.value] + corp_cnt[dropdown.value] });
-        dropdown_delete.options.Add (new Dropdown.OptionData { text = corpAry[dropdown.value] });
-        //~~~~dellete drop down
+            // ドロップダウンに会社名を追加
+            //dropdown_delete.options.Add (new Dropdown.OptionData { text = corpAry[dropdown.value] + corp_cnt[dropdown.value] });
+            dropdown_delete.options.Add (new Dropdown.OptionData { text = corpAry[dropdown.value] });
+            //~~~~dellete drop down
 
-        //add column
-        // 器になるゲームオブジェクトを作成
-        // 引数はオブジェクト名
-        GameObject corp = new GameObject (corp_name_list[corp_name_list.Count - 1]);
+            //add column
+            // 器になるゲームオブジェクトを作成
+            // 引数はオブジェクト名
+            GameObject corp = new GameObject (corp_name_list[corp_name_list.Count - 1]);
 
-        // 作ったゲームオブジェクトをCanvasの子にする
-        corp.transform.parent = GameObject.Find ("Canvas").transform;
-        
-        // 画像のアンカーポジションを追加
-        corp.AddComponent<RectTransform> ().anchoredPosition = new Vector3 (pos_x, pos_y, 0);
+            // 作ったゲームオブジェクトをCanvasの子にする
+            corp.transform.parent = GameObject.Find ("Canvas").transform;
 
-        // 縮尺を変更
-        corp.GetComponent<RectTransform> ().localScale = new Vector3 (0.6f, 0.6f, 0.6f);
+            // 画像のアンカーポジションを追加
+            corp.AddComponent<RectTransform> ().anchoredPosition = new Vector3 (pos_x, pos_y, 0);
 
-        // スプライト画像追加
-        corp.AddComponent<Image> ().sprite = Resources.Load<Sprite> (dropdown.value.ToString ());
+            // 縮尺を変更
+            corp.GetComponent<RectTransform> ().localScale = new Vector3 (0.6f, 0.6f, 0.6f);
 
-        // アスペクト比を元画像と同じサイズにする
-        corp.GetComponent<Image> ().preserveAspect = true;
+            // スプライト画像追加
+            corp.AddComponent<Image> ().sprite = Resources.Load<Sprite> (dropdown.value.ToString ());
 
-        // 画像のwidthとhightを変更
-        //corp.sizeDelta = new Vector2 (50.0f, 50.0f);
-        pos_x = pos_x + 55;
-        if (num == 6) {
-            pos_y = pos_y - 65;
-            pos_x = 320;
+            // アスペクト比を元画像と同じサイズにする
+            corp.GetComponent<Image> ().preserveAspect = true;
+
+            // 画像のwidthとhightを変更
+            //corp.sizeDelta = new Vector2 (50.0f, 50.0f);
+            pos_x = pos_x + 55;
+            if (num == 6) {
+                pos_y = pos_y - 65;
+                pos_x = 320;
+            }
+            dropdown_delete.RefreshShownValue ();
+
         }
-        dropdown_delete.RefreshShownValue ();
     }
 
     public void CorpClickDelete () {
