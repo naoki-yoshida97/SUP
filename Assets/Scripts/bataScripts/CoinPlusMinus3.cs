c3a7b0636c5ac6737ee4c8fee5ecf639a43eca12
junya <>
2020-10-29 19:44:32 +0900

    計算土台

--- Assets/Scripts/bataScripts/CoinPlusMinus3.cs
+++ Assets/Scripts/bataScripts/CoinPlusMinus3.cs
@@ -6,17 +6,16 @@ using UnityEngine.SceneManagement;
 using Photon.Pun;
 using Photon.Realtime;
 
-public class PlusMinusButton2 : MonoBehaviour
-{
+public class CoinPlusMinus3 : MonoBehaviour{
     public Text text;
-    public int num = 3;
+    public int num = 0;
 
     void Start(){
         text = text.GetComponent<Text> ();
     }
 
     public void OnClickPlus(){
-        if(num < 7){
+        if(num < 12){
             num+=1;
         }
         text.text = num.ToString();
