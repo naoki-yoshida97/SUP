2e592592d19754e68c8d3b72763ef5fd66be9397
junya <>
2020-11-08 19:32:35 +0900

    今日の分終わり

--- Assets/Scripts/bataScripts/OpenEvent.cs
+++ Assets/Scripts/bataScripts/OpenEvent.cs
@@ -24,13 +24,12 @@ public class OpenEvent : MonoBehaviour{
 
 
         Debug.Log("押された!");  // ログを出力
+
         GameObject corp = new GameObject(var);
 
         // 作ったゲームオブジェクトをCanvasの子にする
-        corp.transform.parent = GameObject.Find ("Pane-ra").transform;
-
-        // 作ったゲームオブジェクトをPanelの子にする
-        //corp2.transform.parent = corp2.transform.parent.Find ("Pane-ra").transform;
+        //corp.transform.parent = GameObject.Find ("Canvas_1").transform;
+        //corp.transform.parent = GameObject.Find ("Pane-ra").transform;
 
         int[] list={89,90,91};
         for (int j = 0; j < list.Length; j++){
@@ -38,20 +37,24 @@ public class OpenEvent : MonoBehaviour{
         }
         Debug.Log(list.Length);
         for(int i = 0; i < 4; i++){
-            int v = -300;
+            int v = -400;
+
+            corp.transform.parent = GameObject.Find ("Pane-ra").transform;
+
             // 画像のアンカーポジションを追加
             corp.AddComponent<RectTransform> ().anchoredPosition = new Vector3 (v, 50, 0);
 
             // 縮尺を変更
-            corp.GetComponent<RectTransform> ().localScale = new Vector3 (2, 2, 2);
+            corp.GetComponent<RectTransform> ().localScale = new Vector3 (2, 2, 0);
 
             // スプライト画像追加
             corp.AddComponent<Image> ().sprite = Resources.Load<Sprite>(list[i].ToString());
 
             // アスペクト比を元画像と同じサイズにする
             corp.GetComponent<Image> ().preserveAspect = true;
-            v += 100;
-            Debug.Log("表示しますわ(>_<)#");
+            v += 200;
+            Debug.Log(list[i]);
+            Debug.Log("表示するしゅば!(>_<)#");
         }
         // 画像のアンカーポジションを追加
         //corp.AddComponent<RectTransform> ().anchoredPosition = new Vector3 (0, 0, 0);
