301bf60c30611080415a2c234a95eec8df05a0c4
1811140054k <1811140054k@ed.fuk.kindai.ac.jp>
2021-03-12 15:26:08 +0900

    試しスクリプト達

---
+++ Assets/Scripts/shutoScript/PlayerList.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+
+public class PlayerList : MonoBehaviour
+{
+    public void Awake()
+    {
+        // 適当にニックネームを設定
+        //PhotonNetwork. = "guest" + UnityEngine.Random.Range(1000, 9999);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
