0dd39a25896392e41baedf3e97178da04b0f6764
1811140054k <1811140054k@ed.fuk.kindai.ac.jp>
2021-03-25 18:01:01 +0900

    ローカルで名前をキャラクターの上に表示させながら移動もさせる

---
+++ Assets/Scripts/shutoScript/OperationName.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OperationName : MonoBehaviour
+{
+    private GameObject namePlate; //名前を表示しているプレート
+    public Text nameText;   //名前を表示するテキスト
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        namePlate = nameText.transform.parent.gameObject;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        namePlate.transform.rotation = Camera.main.transform.rotation; //まぁ2Dだからいらないけどね
+        nameText.text = name;
+    }
+}
