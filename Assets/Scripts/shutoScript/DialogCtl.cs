ceac72373fe57b61d74a51efde1d4571e96e8e2c
1811140054k <1811140054k@ed.fuk.kindai.ac.jp>
2021-11-07 19:05:20 +0900

    メニュー＆会社一覧ダイアログ作成

---
+++ Assets/Scripts/shutoScript/DialogCtl.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogCtl : MonoBehaviour
+{
+    //メニューダイアログの操作↓
+    public void MenuDialogShow()
+    {
+        GameObject menuDialog = GameObject.Find("menuDialog");
+        menuDialog.transform.localScale = new Vector3(1,1,1);
+    }
+
+    public void MenuDialogHide()
+    {
+        GameObject menuDialog = GameObject.Find("menuDialog");
+        menuDialog.transform.localScale = new Vector3(0,0,0);
+    }
+    //--------------------------
+
+    //起業ダイアログ操作
+    public void CompanyDialogShow()
+    {
+        GameObject menuDialog = GameObject.Find("CompanyDialog");
+        menuDialog.transform.localScale = new Vector3(1,1,1);
+    }
+
+    public void CompanyDialogHide()
+    {
+        GameObject menuDialog = GameObject.Find("CompanyDialog");
+        menuDialog.transform.localScale = new Vector3(0,0,0);
+    }
+
+    //--------------------------
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
